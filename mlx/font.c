7ac5f71fada865a4048ad8ded1f87abeb00c3fe1
Elvira Martínez López <elvmarti@MacBook-Pro-de-Carlos.local>
2021-06-24 14:41:30 +0200

    fractol

--- mlx/font.c
+++ mlx/font.c
@@ -3,7 +3,7 @@
 static const struct {
   unsigned int 	 width;
   unsigned int 	 height;
-  unsigned int 	 bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */ 
+  unsigned int 	 bytes_per_pixel; //2:RGB16, 3:RGB, 4:RGBA
   unsigned char	 pixel_data[1140 * 20 * 4 + 1];
 } font_atlas = {
   1140, 20, 4,
